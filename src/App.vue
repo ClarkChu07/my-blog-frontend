fd20b7ee8a396be7f150a69e0a0e5e3753640b50
chuchao <835458804@qq.com>
2022-01-18 11:08:19 +0800

    feat:更换首页粒子特效

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,29 @@
 <template>
   <el-container>
     <el-header class="header-flex">
-      <div>
-        <el-avatar
-          :src="Images.logoUrl"
-          fit="contain"
-          shape="square"
-          size="default"
-        ></el-avatar>
-      </div>
-      <div>
-        <el-menu
-          class="el-menu-demo"
-          mode="horizontal"
-          :ellipsis="isCollapse"
-          router
-          :default-active="$route.path"
-          active-text-color="#409EFF"
-        >
-          <el-menu-item index="/Home">首页</el-menu-item>
-          <el-menu-item disabled index="/Article">文章</el-menu-item>
-          <el-menu-item disabled index="/Project">项目</el-menu-item>
-          <el-menu-item disabled index="/Message">留言</el-menu-item>
-          <el-menu-item disabled index="/About">关于</el-menu-item>
-        </el-menu>
-      </div>
+      <el-avatar
+        :src="Images.logoUrl"
+        fit="contain"
+        shape="square"
+        size="default"
+      ></el-avatar>
+      <el-menu
+        mode="horizontal"
+        router
+        default-active="/Home"
+        active-text-color="#409EFF"
+        :ellipsis="isCollapse"
+      >
+        <el-menu-item index="/Home">首页</el-menu-item>
+        <el-menu-item disabled index="/Article">文章</el-menu-item>
+        <el-menu-item disabled index="/Project">项目</el-menu-item>
+        <el-menu-item disabled index="/Message">留言</el-menu-item>
+        <el-menu-item index="/About">关于</el-menu-item>
+      </el-menu>
     </el-header>
-    <div class="main-style">
+    <el-main>
       <router-view :key="key" />
-    </div>
-    <!-- <el-footer>Design By ClarkChu</el-footer> -->
+    </el-main>
   </el-container>
 </template>
 <script lang="ts">
@@ -50,24 +44,13 @@ export default defineComponent({
     }
   },
   setup() {
-    const activeIndex = "1";
     const isCollapse = false;
     const Images = {
       logoUrl,
     };
 
-    // axios
-    //     .get("/users/XPoet")
-    //     .then((res) => {
-    //       console.log("res: ", res);
-    //     })
-    //     .catch((err) => {
-    //       console.log("err: ", err);
-    //     });
-
     return {
       Images,
-      activeIndex,
       isCollapse,
     }
   }
@@ -78,6 +61,7 @@ html,
 body {
   font-size: 16px;
   width: 100%;
+  height: 100%;
   margin: 0;
   padding: 0;
 }
@@ -103,19 +87,11 @@ a {
   line-height: 60px;
 }
 
-.el-footer{
-  position: fixed;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.main-style {
+.el-main {
   border-top: 1px solid #f2f2f2;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   position: relative;
-  padding: 0;
 }
 
 .header-flex {
@@ -124,6 +100,7 @@ a {
   align-items: center;
   width: 50%;
   margin: 0 10%;
+  z-index: 99;
 
   div:first-child {
     display: flex;
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import store from './store'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
+import Particles from 'particles.vue3'
 
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
 app.use(ElementPlus)
+app.use(Particles)
 app.mount('#app')
